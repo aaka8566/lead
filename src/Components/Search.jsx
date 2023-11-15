67417578d9ec9616c19811d98a408311b37b8189
Aakash Chakravarty <112715657+aaka8566@users.noreply.github.com>
2023-11-15 23:54:08 +0530

    Pincode app

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -2,6 +2,7 @@ import { Box,Button,FilledInput, Stack, TextField} from '@mui/material';
 import { useEffect, useState } from 'react';
 import {useSelector,useDispatch} from 'react-redux';
 import { RESET, getalllocationsuccess } from '../Redux/action';
+import LoadingButton from '@mui/lab/LoadingButton';
 const Search = () => {
     const [code,setcode]=useState("");
     const {location ,isloading}=useSelector((store)=>store. locationreducer);
@@ -43,10 +44,13 @@ useEffect(()=>{
              helperText="Please enter any Pincode"
              onChange={(e)=>setcode(e.target.value)}
            />
-       <Box sx={{border:"0px solid pink",display:"flex",justifyContent:"center"}}><Button sx={{cursor:"pointer"}} type='submit' variant="contained" >Search</Button></Box>
+       <Box sx={{border:"0px solid pink",display:"flex",justifyContent:"center"}}><LoadingButton loading={isloading?true:false} sx={{cursor:"pointer"}} type='submit' variant="contained" >Search</LoadingButton></Box>
         </form>
         <Box sx={{border:"0px solid pink",display:"flex",justifyContent:"center"}}><Button sx={{cursor:"pointer",backgroundColor:'red'}} onClick={()=>{setcode("");dispatch(RESET());}} variant="contained" >Reset</Button></Box>
         </Stack>
+        {/* <LoadingButton loading={false} variant="outlined">
+  Submit
+</LoadingButton> */}
     </Box>
   )
 }
